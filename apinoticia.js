76577897d135fd7f8df16569e2a5964e9cfdcc56
pucminasCC <gabriel.carvalho.1327250@sga.pucminas.br>
2021-06-19 01:59:20 -0300

    trabalho pika

---
+++ apinoticia.js
@@ -0,0 +1,7 @@
+$.getJSON(
+    "https://newsapi.org/v2/top-headlines?sources=google-news-br&apiKey=f8c577b67e3e4f11b591a2c4d8ed4ed7",
+    function(data) {
+        console.log(data);       
+        
+    }
+);
